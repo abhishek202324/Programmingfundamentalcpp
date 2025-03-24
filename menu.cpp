d6fa9ad804e0a286f531af9d1714a8f2efb5ea97
ABHISHEK KUMAR <170253904+abhishek202324@users.noreply.github.com>
2025-03-24 22:22:35 +0530

    Add files via upload

---
+++ menu.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int choice;
+    cout<<"Enter the choice \n press 1 for calculate ";
+    cin>>choice;
+    switch (choice)
+    {
+    case 1:
+        
+        break;
+    
+    default:
+        break;
+    }
+}
