8dae0ea4675dc637e193eb7fc7075c67f67b55fd
ABHISHEK KUMAR <170253904+abhishek202324@users.noreply.github.com>
2025-03-29 18:29:47 +0530

    Add files via upload

---
+++ nestedLoop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<10;i++){ //for row 
+        for(int j=0;j<5;j++){  //for collum
+        //cout<<i<<" ";
+        cout<<j<<" ";
+    }
+    cout<<endl;
+    }
+
+}
