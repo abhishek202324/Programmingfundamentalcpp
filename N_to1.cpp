049ef80e4ca6998dbaf3fc39fc6fe98b41506619
ABHISHEK KUMAR <170253904+abhishek202324@users.noreply.github.com>
2025-04-06 13:34:26 +0530

    Add files via upload

---
+++ N_to1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    int n;
+    cout<<"Enter the number N";
+    cin>>n;
+
+    for(i=n;i>=1;i--){
+        cout<<i<<endl;
+    }
+}
